7bad20fac5f3c062cdd1c6a4c47bad8dc73f526c
Jean Pereira <jeankpayales444@gmail.com>
2025-06-18 12:02:07 -0600

    writes data to CSV and JSON

--- file_writer.py
+++ file_writer.py
@@ -4,8 +4,20 @@ import json
 
 def write_to_csv(data, filename="weather_data.csv"):
     # TODO: Write or append to CSV with headers
-    pass
+    file_exists = os.path.isfile(filename)
+    with open(filename, mode="a", newline="", encoding="utf-8") as f:
+        writer = csv.DictWriter(f, fieldname=data[0].keys())
+        if not file_exists:
+            writer.writeheader()
+        writer.writerows(data)
+
 
 def write_to_json(data, filename="weather_data.json"):
     # TODO: Read existing JSON (if any), append data, and write back
-    pass
+    existing = []
+    if os.path.isfile(filename):
+        with open(filename, "r", encoding="utf-8") as f:
+            existing = json.load(f)
+    existing.extend(data):
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(existing, f, ensure_ascii=False, indent=2)
